e1e2c6c036b0dd6c3b1cd8ab3a82745aa6d09156
Claaarree <clare.fh@hotmail.com>
2025-03-21 09:35:17 +0800

    task 1

--- data/restaurant.sql
+++ data/restaurant.sql
@@ -9,7 +9,10 @@ use restaurant;
 
 create table customers (
   username varchar(64) not null,
-  password varchar(128) not null
+  password varchar(128) not null,
+-- Need to have a primary key in order to reference it as
+-- a foreign key in the place_orders table
+  constraint pk_username primary key (username)
 );
 
 insert into customers(username, password) values
@@ -21,3 +24,18 @@ insert into customers(username, password) values
 
 -- TODO: Task 1.2
 -- Write your task 1.2 below
+
+create table place_orders (
+  order_id char(8) not null,
+  payment_id varchar(128) not null,
+  order_date date not null,
+  total decimal(8,2) not null,
+  username varchar(64) not null,
+
+  constraint pk_order_id primary key (order_id), 
+  constraint fk_username foreign key (username)
+    references customers(username)
+);
+
+grant all privileges on restaurant.* to 'fred'@'%';
+flush privileges;
